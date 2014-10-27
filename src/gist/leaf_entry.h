bb7cdc475d9596bedb4150a5f05e4776df185305
girafffeg <alina.dubatovka@gmail.com>
2014-10-27 18:19:24 +0400

    Gist::search() is implemented, Entry::find() is added

--- src/gist/leaf_entry.h
+++ src/gist/leaf_entry.h
@@ -1,11 +1,23 @@
 #pragma once
 
 #include <vector>
-
 #include "gist/entry.h"
 
+
 template <typename P>
-class LeafEntry<P> : public Entry<P> {
+class LeafEntry : public Entry<P> {
 private:
     void* data;
+
+    std::vector<LeafEntry<P> *> find(P const &predicate) {
+		if (!predicate.consistentWith(this.getPredicate())) {
+			return std::vector<LeafEntry<P> *> ();
+		}
+
+		std::vector<LeafEntry<P> *> result;
+		result.push_back(this);
+
+		return result;
+	}
+
 };
