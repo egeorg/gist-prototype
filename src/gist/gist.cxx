8a4d88e0f20de9d1e815006c0ea84a987ed3bc09
girafffeg <alina.dubatovka@gmail.com>
2014-11-10 21:51:56 +0400

    Gist::insert() is implemented with some private methods

--- src/gist/gist.cxx
+++ src/gist/gist.cxx
@@ -22,19 +22,20 @@ template <typename P>
 std::vector<LeafEntry<P> *> Gist<P>::search(const P &predicate) const {
     std::vector<LeafEntry<P> *> result;
 	std::stack<Entry<P> *> entryStack;
-	entryStack.push_back(root);
+	entryStack.push(root);
 	while (!entryStack.empty()) {
-		Entry<P> *curEntry = entryStack.pop_back();
+		Entry<P> *curEntry = entryStack.top();
+        entryStack.pop();
 		std::vector<Entry<P> *> children = curEntry -> getChildren();
 
 		if (children.empty()) {
-			result.push_back(curEntry);	
+			result.push_back(static_cast<LeafEntry<P>*> (curEntry));
 			continue;
 		}
 		
 		for (typename std::vector<Entry<P> *>::iterator child = children.begin(); child != children.end(); ++child) {
-			if (predicate.consistentWith(child -> predicate)) {
-				entryStack.push_back(child);
+			if (predicate.consistentWith((*child) -> getPredicate())) {
+				entryStack.push(*child);
 			}
 
 		}
@@ -44,7 +45,54 @@ std::vector<LeafEntry<P> *> Gist<P>::search(const P &predicate) const {
 }
 
 template <typename P>
-void Gist<P>::insert(Entry<P> const &e) {
+InnerEntry<P> Gist<P>::chooseSubtree(LeafEntry<P> const &E, int l) {
+    P predicate = E.getPredicate();
+    Entry<P> *curEntry = root;
+
+	while (true) {
+		if (curEntry->getLevel() == l) {
+			return *static_cast<InnerEntry<P>*>(curEntry);
+		}
+		
+		std::vector<Entry<P> *> children = curEntry->getChildren();
+		
+		Entry<P> *bestChild = *children.begin();
+		double bestPenalty = predicate.penalty(bestChild->getPredicate());
+		for (typename std::vector<Entry<P> *>::iterator child = children.begin() + 1; child != children.end(); ++child) {
+			double curPenalty = predicate.penalty(static_cast<InnerEntry<P>*>(*child)->getPredicate());
+			if (curPenalty < bestPenalty) {
+				bestPenalty = curPenalty;
+				bestChild = *child;
+			}
+		}
+		curEntry = bestChild;
+	}
+}
+
+template <typename P>
+void Gist<P>::split(InnerEntry<P> &L, LeafEntry<P> const &E) {
+	Set1, Set2 = E.getPredicate().pickSplit(L.getSubpredicates());
+}
+
+template <typename P>
+void Gist<P>::adjustKeys(InnerEntry<P> &L) {
+	for (InnerEntry<P> curEntry = L; curEntry.getParent() != nullptr; curEntry = *(curEntry.getParent())) {
+		curEntry.setPredicate(*(new P(curEntry.getSubpredicates())));
+	}
+}
+
+template <typename P>
+void Gist<P>::insert(const LeafEntry<P> &E, int l = 0) {
+	InnerEntry<P> L = chooseSubtree(E, l);
+	
+	if (L.getChildren().size() < max_fanout)
+	{
+		L.insert(E);
+		return;
+	}
+
+	split(L, E);
+	adjustKeys(L);
 }
 
 template <typename P>
--- src/gist/gist.h
+++ src/gist/gist.h
@@ -14,12 +14,15 @@ private:
     int min_fanout;
     InnerEntry<P> *root;
 
+    InnerEntry<P> chooseSubtree(LeafEntry<P> const &E, int l);
+    void split(InnerEntry<P> &L, LeafEntry<P> const &E);
+    void adjustKeys(InnerEntry<P> &L);
 public:
     Gist(int max_fanout, int min_fanout);
     virtual ~Gist();
 
     std::vector<LeafEntry<P> *> search(const P &p) const;
-    void insert(const Entry<P> &e);
+    void insert(const LeafEntry<P> &E, int l = 0);
     void deleteEntry(const Entry<P> &e);
 };
 
