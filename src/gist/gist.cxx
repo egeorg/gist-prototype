33639422f40cc4675173e966a2299df0acfb23f5
girafffeg <alina.dubatovka@gmail.com>
2014-11-18 01:24:52 +0400

    New Gist::insert() without Entries' parents. New InnerEntry without parent.
    Gist::chooseSubtree(), Gist::split(), Gist::adjustKeys() are deleted and merged into Gist::insert().

--- src/gist/gist.cxx
+++ src/gist/gist.cxx
@@ -3,6 +3,7 @@
 #include "gist/gist.h"
 #include <stack>
 #include <utility>
+
 template <typename P>
 Gist<P>::Gist(int u, int l) {
     max_fanout = u;
@@ -13,7 +14,7 @@ Gist<P>::Gist(int u, int l) {
 template <typename P>
 Gist<P>::~Gist() {
 	if (root != nullptr) {
-		// TODO
+		//TODO
 		delete root;
 	}
 }
@@ -44,61 +45,46 @@ std::vector<LeafEntry<P> *> Gist<P>::search(const P &predicate) const {
 }
 
 template <typename P>
-InnerEntry<P> Gist<P>::chooseSubtree(LeafEntry<P> const &E) {
+void Gist<P>::insert(Entry<P> E) {
     P predicate = E.getPredicate();
-    Entry<P> *curEntry = root;
+    std::stack<InnerEntry<P>*> path;
 
-	while (true) {
-		std::vector<Entry<P> *> children = curEntry->getChildren();
-		Entry<P> *bestChild = *children.begin();
+    for (InnerEntry<P> *curEntry = root;;) {
+        path.push(curEntry);
+        std::vector<Entry<P> *> children = curEntry->getChildren();
+        Entry<P> *bestChild = *children.begin();
 
-		if (bestChild->getChildren().empty()) {
-			return *static_cast<InnerEntry<P>*>(curEntry);
-		}
+        if (bestChild->getChildren().empty()) {
+            break;
+        }
 
-		double bestPenalty = predicate.penalty(bestChild->getPredicate());
-		for (typename std::vector<Entry<P> *>::iterator child = children.begin() + 1; child != children.end(); ++child) {
-			double curPenalty = predicate.penalty(static_cast<InnerEntry<P>*>(*child)->getPredicate());
-			if (curPenalty < bestPenalty) {
-				bestPenalty = curPenalty;
-				bestChild = *child;
-			}
-		}
-		curEntry = bestChild;
-	}
-}
-
-template <typename P>
-void Gist<P>::split(InnerEntry<P> &L, Entry<P> const &E) {
-    std::pair<std::vector<P &>, std::vector<P &> > sets = E.getPredicate().pickSplit(L.getSubpredicates());
-    L.setChildren(sets.first);
-    InnerEntry <P> N = *(new InnerEntry<P> (sets.second));
-
-    InnerEntry<P> *R = L.getParent();
-    if (R->getChildren().size() < max_fanout) {
-        R->insert(N);
-        return;
+        double bestPenalty = predicate.penalty(bestChild->getPredicate());
+        for (typename std::vector<Entry<P> *>::iterator child = children.begin(); child != children.end(); ++child) {
+            double curPenalty = predicate.penalty(static_cast<InnerEntry<P>*>(*child)->getPredicate());
+            if (curPenalty < bestPenalty) {
+                bestPenalty = curPenalty;
+                bestChild = *child;
+            }
+        }
+        curEntry = static_cast<InnerEntry<P>*>(bestChild);
     }
-}
 
-template <typename P>
-void Gist<P>::adjustKeys(InnerEntry<P> &L) {
-	for (InnerEntry<P> curEntry = L; curEntry.getParent() != nullptr; curEntry = *(curEntry.getParent())) {
-		curEntry.setPredicate(*(new P(curEntry.getSubpredicates())));
-	}
-}
+    for (InnerEntry<P> *L = path.top();; L = path.top()) {
+        if (L->getChildren().size() < max_fanout) {
+            L->insert(E);
+            break;
+        }
+        path.pop();
+        std::pair<std::vector<P &>, std::vector<P &> > sets = E.getPredicate().pickSplit(L->getSubpredicates());
+        L->setChildren(sets.first);
+        L->setPredicate(*(new P(L->getSubpredicates())));
+        E = *(new InnerEntry<P> (sets.second));
+    }
 
-template <typename P>
-void Gist<P>::insert(const LeafEntry<P> &E) {
-	InnerEntry<P> L = chooseSubtree(E);
-
-	if (L.getChildren().size() < max_fanout) {
-		L.insert(E);
-		return;
-	}
-
-	split(L, E);
-	adjustKeys(L);
+    for (InnerEntry<P>* curEntry = path.top(); !path.empty(); curEntry = path.top()) {
+        curEntry->setPredicate(*(new P(curEntry->getSubpredicates())));
+        path.pop();
+    }
 }
 
 template <typename P>
--- src/gist/inner_entry.h
+++ src/gist/inner_entry.h
@@ -9,23 +9,16 @@ template <typename P>
 class InnerEntry : public Entry<P> {
 private:
     Node<P> node;
-    InnerEntry<P>* parent;
-
 public:
     InnerEntry<P> () {
         node = *(new Node<P>(std::vector<Entry<P> *>()));
         this->predicate = *(new P());
-        parent = nullptr;
     }
 
     InnerEntry<P> (std::vector<Entry<P> *> entries) {
         node = *(new Node<P>(entries));
         this->predicate = *(new P(getSubpredicates()));
     }
-
-	InnerEntry<P>* getParent() {
-		return parent;
-	}
 
     std::vector<Entry<P> *> getChildren() {
         return node.getEntries();
@@ -36,20 +29,17 @@ public:
 		std::vector<P*> result;
 		for (typename std::vector<Entry<P> *>::iterator child = children.begin(); child != children.end(); ++child) {
 			result.push_back((*child) -> getPredicate());
-		}	
+		}
 		return result;
 	}
 
     void setChildren (std::vector<Entry<P> *> entries) {
         node.setEntries(entries);
+        //predicate = *(new P(this->getSubpredicates()));
     }
 
     void insert(const Entry<P> &E) {
         E.setParent(this);
         node.insert(E);
-    }
-
-    void setParent(InnerEntry<P>* E) {
-        parent = E;
     }
 };
